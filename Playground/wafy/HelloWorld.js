358bb77be9ad65f124e746e9e8afa5084add83d8
fmwf <fmwafii@gmail.com>
2022-07-22 15:09:33 +0700

    feat: task 1 Wafi

--- Playground/wafy/HelloWorld.js
+++ Playground/wafy/HelloWorld.js
@@ -1,1 +1,5 @@
-console.log
+console.log ("lalalalalalala")
+console.log ("Nama: Wafy")
+console.log ("Domisili: Jakarta)
+console.log ("Tanggal Lahir: 16 Mei 1999")
+console.log ("Jenis Kelamin: laki-laki")
