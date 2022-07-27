76a7c966ed9d56f210f84ec529d51564d1038f75
shadowescape <donnyfernando280@gmail.com>
2022-07-27 21:29:55 +0700

    feat: upload task data-types.js

---
+++ Playground/donny/2.Data-Types.js
@@ -0,0 +1,8 @@
+var nama_depan = 'Donny'
+var nama_belakang = 'Fernando'
+var umur = 24
+var menikah = 'belum menikah'
+var tanggal_lahir = new Date(1998, 2, 5)
+var jenis_kelamin = 'Laki-laki'
+
+console.log('Nama saya adalah ' + nama_depan + ' ' + nama_belakang + ', ' + 'saya berjenis kelamin' + jenis_kelamin + '. ' + 'Saya lahir pada tanggal ' + tanggal_lahir + ' dan saya sekarang berumur ' + umur + ' tahun dan ' + menikah)
